abff6b1e7e24f99dd07917290ffb6687cb2204ed
EdwardObala1 <e.obala@alustudent.com>
2022-03-05 13:09:45 +0200

    passions screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 // ignore: library_prefixes
 import 'package:latlong2/latlong.dart' as latLng;
+import './screens/onboarding/onboarding1.dart';
 
 void main() {
   runApp(const MyApp());
